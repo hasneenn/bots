24f6c59577723be92f723c11d6b6f8e570f5967b
hasneenn <142596332+hasneenn@users.noreply.github.com>
2023-12-08 13:17:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,31 @@
-import telebot,time,requests,re
-from telebot import types as t
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from datetime import datetime
+from telethon.sync import TelegramClient, events
+from telethon.tl.functions.account import UpdateProfileRequest
+import time
+import datetime
+import time
+import requests
+ 
+api_id = 16748685
+api_hash = 'f0c8f7e4a7a50b5c64fd5243a256fd2f'
 
-see = requests.Session()
+with TelegramClient(StringSession('1BJWap1wBu2E1jhX4RILc2DWBY1QHSTp69CzsYRjOttaJGvC8xdtuqSSAXceHEBV9sZ00tHMi7uAIZuP9xasQPtXg2cyqiG16RuYdv141oeP6oJlv6BdpSUSLugOQ9VJTULYesE5hZyk0FRMYm1z3t4np2-mPegAiVzxJiVnOPa46lWynxBRYNJ_HjSocr0nPS4-cj4HN4KeG2HZXE9QhmcbKLJOBoTB9XrjVUblYBgx0sfCfVvI3kVfskRVP8Z9uAZx7F1vOI6AlWAtmUXSkmVSB9ccQDIpVw22TFkn_t6VMXKL_Z0HOSrqaoLJeFMAnr7UavaRIvn4PhU4X4gEHSswEvwoYvB0='), api_id, api_hash) as client:
+    print(client.session.save())
+    
 
-der = telebot.TeleBot('6330234461:AAENRLIRpqmTDL_X-RqKWslq3PHVO6K1XEM')
 
-@der.message_handler(commands=['start'])
-def start(mes):
-	d = t.InlineKeyboardButton('قناتي انضم يحلوو', url='t.me/llxxx3')
-	de = t.InlineKeyboardMarkup(row_width=2);de.add(d)
-	na = mes.from_user.first_name
-	acc = der.send_message(mes.chat.id,text=f'<b>Halo {na}, \nthis bot made @llxxx3\n\nEnter username:password</b>', reply_markup=de,parse_mode='HTML')
-	der.register_next_step_handler(acc,ax)
-def ax(mes):
-		ac = str(mes.text)
-		try:
-			user = ac.split(':')[0]
-			pasw = ac.split(':')[1]
-		except IndexError as error:
-			d = t.InlineKeyboardButton('قناتي انضم يحلوو', url='t.me/llxxx3')
-			de = t.InlineKeyboardMarkup(row_width=2);de.add(d)
-			
-			der.send_message(mes.chat.id,text='عذرا عزيزي 📛\nلا يمكنك الارسال على هذه النحو',reply_markup=de)
-			if not (':') in ac:
-				return
-		zo = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
-		zh = '1234567890'
-		import random
-		us="".join(random.choice(zo)for i in range(6))
-		ud="".join(random.choice(zo)for i in range(4))
-		udm="".join(random.choice(zh)for i in range(4))
-		ua=f'{us}/9.80 (Series 60; {us} {ud}/7.0.{udm}00/28.3445; U; en) Presto/2.8.119 Version/11.10'
-		see.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent": ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
-		
-		p = see.get('https://m.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F').text
-	#print(p)
-		
-		dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p)).group(1),"uid":user,"flow":"login_no_pin","pass":pasw,"next":"https://developers.facebook.com/tools/debug/accesstoken/"}
-		
-		see.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent": ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
-		
-		po = see.post('https://m.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,allow_redirects=False)
-		
-		kie = see.cookies.get_dict()
-		
-		if 'xs' and 'c_user' in kie:
-			koe = f"datr={kie['datr']};sb={kie['sb']};vpd=v1%3B633x360x2;locale=ar_AR;m_pixel_ratio=2;fr={kie['fr']};c_user={kie['c_user']};xs={kie['xs']};m_page_voice={kie['c_user']};wd=360x633;"
-			d = t.InlineKeyboardButton('قناتي انضم يحلوو', url='t.me/llxxx3')
-			de = t.InlineKeyboardMarkup(row_width=2);de.add(d)
-			der.send_message(mes.chat.id,text=f'''<strong>
-✅ Good Cookie Facebook 
----------------------------------------------
-<code>{koe}</code>
----------------------------------------------
-@PY_87 - @llxxx3 « انضم</strong>''', parse_mode='html',reply_markup=de)
-		else:
-			d = t.InlineKeyboardButton('قناتي انضم يحلوو', url='t.me/llxxx3')
-			do = t.InlineKeyboardMarkup(row_width=2);do.add(d)
-			der.send_message(mes.chat.id,text='عذرا عزيزي 📛\nخطا في تسجيل الدخول الرجاء إعادة المحاولة لطفاً',reply_markup=do)
-der.polling()
+
+@client.on(events.NewMessage)
+async def upd_name(event):
+    while True:
+        iraq_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3)))
+        hours = divmod(iraq_time.hour, 12)[1]
+        name = f"\r{hours}:{iraq_time.strftime('%M:%S')}"
+        time.sleep(1)
+        await client(UpdateProfileRequest(first_name=name))
+
+
+client.start()
+client.run_until_disconnected()
